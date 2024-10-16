1c52758bc1621f9faa67270b93816c16c86f5435
Adrian Paul <youadrianppaul@gmail.com>
2024-10-16 20:36:04 +0800

    New additions

--- src/components/projects/Filter.jsx
+++ src/components/projects/Filter.jsx
@@ -3,10 +3,12 @@ import Proj4 from "../../assets/camluxe-mockup.png";
 import Proj1 from "../../assets/bpi-website-mockup.png"
 import Proj5 from "../../assets/csol-mockup-2.png"
 import Proj2 from "../../assets/wbst-prtfl.png"
+import Proj6 from "../../assets/flexify-mockup.jpg"
 import capstoneProj from "../../assets/capstonedoc.pdf";
 
 
 const Filter = [
+
     {
         id: 1,
         image: Proj4,
@@ -17,14 +19,6 @@ const Filter = [
         description: "Camluxe is a platform I designed for photographers to share their work in a clean and beautiful way. It features four categories—Portrait, Fashion, Landscape, and Lifestyle—so users can easily view or organize their photos and let their creativity stand out.",
     },
     {
-        id: 2,
-        image: Proj2,
-        title: "Website Portfolio",
-        category: ["frontend", "html", "css", "reactjs"],
-        urlgithub: "https://github.com/imadrianpaul/adrianpinks-website.git",
-        description: "A personal website portfolio to showcase projects and skills using HTML5, CSS3, and ReactJS. The website features a clean, responsive design. The site is hosted on Vercel for seamless performance and accessibility."
-    },
-    {
         id: 3,
         image: Proj3,
         title: "The E-commerce Lunarshift Case Study",
@@ -32,6 +26,24 @@ const Filter = [
         urlfigma: "https://www.figma.com/proto/fkFWVuS017CpeHWeyeDnxw/Lunarshift-%7C-E-commerce-Website?node-id=880-4342&starting-point-node-id=880%3A4342",
         urlstudy: "https://www.figma.com/proto/fkFWVuS017CpeHWeyeDnxw/Lunarshift-%7C-E-commerce-Website?node-id=1157-7803&scaling=scale-down-width&content-scaling=fixed",
         description: "LunarShift is a tech-focused e-commerce site offering high-performance gaming peripherals with a cosmic theme. Its purpose is to provide gamers with top-quality gear while creating an immersive, visually striking shopping experience."
+    },
+    {
+        id: 6,
+        image: Proj6,
+        title: "Flexify",
+        category: ["design", "ui/ux"],
+        urlgithub: "https://github.com/imadrianpaul",
+        urlfigma: "",
+        urlstudy: "/",
+        description: "Flexify is a modern banking app that simplifies financial management with an intuitive interface for tracking expenses and managing cards. Case study coming soon."
+    },
+    {
+        id: 2,
+        image: Proj2,
+        title: "Website Portfolio",
+        category: ["frontend", "html", "css", "reactjs"],
+        urlgithub: "https://github.com/imadrianpaul/adrianpinks-website.git",
+        description: "A personal website portfolio to showcase projects and skills using HTML5, CSS3, and ReactJS. The website features a clean, responsive design. The site is hosted on Vercel for seamless performance and accessibility."
     },
     {
         id: 4,
@@ -49,7 +61,8 @@ const Filter = [
         urlgithub: "https://github.com/imadrianpaul",
         urlstudy: capstoneProj,
         description: " The system helps IT technicians track hardware statuses in real-time, detect issues early, and manage network resources through a visual mapping feature. It reduces their workload and ensures optimal computer performance."
-    },
+    }
+
     
 ];
 
